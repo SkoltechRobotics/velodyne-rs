c7874eb023857c08895b41e058db86838996f71f
newpavlov <newpavlov@gmail.com>
2019-02-06 20:19:13 +0300

    fix benches

--- benches/mod.rs
+++ benches/mod.rs
@@ -8,10 +8,23 @@ use velodyne::packet::PcapSource;
 use test::Bencher;
 
 #[bench]
-fn turn_iterator(b: &mut Bencher) {
-    let path = "/media/newpavlov/DATA/oscar/velodyne.pcap";
+fn bench_hdl64_turn(b: &mut Bencher) {
+    let path = "data/hdl64.pcap";
     let source = PcapSource::new(path, false, true).unwrap();
     let mut turn_iter = TurnIterator::hdl64_init(source).unwrap();
+
+    b.iter(|| {
+        let res = turn_iter.next().unwrap();
+        let point: Vec<FullPoint> = res.unwrap().1;
+        test::black_box(point);
+    });
+}
+
+#[bench]
+fn bench_hdl32_turn(b: &mut Bencher) {
+    let path = "data/hdl32.pcap";
+    let source = PcapSource::new(path, false, true).unwrap();
+    let mut turn_iter = TurnIterator::hdl32_init(source);
 
     b.iter(|| {
         let res = turn_iter.next().unwrap();
